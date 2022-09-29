99faddc5230ef4cb595fc81616625438e223b8ca
DevandraNabana20 <yumandaevan@gmail.com>
2022-09-29 16:40:52 +0700

    ProjectOOP

---
+++ src/Process/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Process;
+import Process.Login;
+/**
+ *
+ * @author yuman
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login frame = new Login();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    
+}
---
+++ src/Process/RentalInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Process;
+
+/**
+ *
+ * @author yuman
+ */
+public interface RentalInterface {
+   public void Kirimdata(String x);
+   
+    
+}
